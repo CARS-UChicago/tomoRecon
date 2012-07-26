ce3589882c41f565713c6f2a9114e8268633d0c5
Mark Rivers <rivers@cars.uchicago.edu>
2012-07-26 21:02:21 +0000

    Merger tomoSupervisor and tomoWorker classes back into tomoRecon class; added logMsg; bug fixes

--- tomoReconApp/src/tomoRecon.cpp
+++ tomoReconApp/src/tomoRecon.cpp
@@ -13,69 +13,36 @@
  */
 #include <stdlib.h>
 #include <math.h>
+#include <stdarg.h>
 
 #include <epicsTime.h>
 
 #include "tomoRecon.h"
 
-static tomoSupervisor *pTomoSupervisor = NULL;
-static int reconComplete;
-static int slicesRemaining;
-static int tomoDebug;
-static FILE *debugFile;
-static int tomoAbort;
 
 extern "C" {
-int tomoReconStart(tomoParams_t *pTomoParams, float *pAngles, float *pInput, float *pOutput)
+static void supervisorTask(void *pPvt)
 {
-  if (pTomoSupervisor) {
-    fprintf(debugFile, "tomoReconStart: tomoSupervisor object already exists\r\n");
-    fflush(debugFile);
-    return -1;
-  }
-  pTomoSupervisor = new tomoSupervisor(pTomoParams, pAngles, pInput, pOutput);
-  return 0;
+  tomoRecon *ptomoRecon = (tomoRecon *)pPvt;
+  ptomoRecon->supervisorTask();
 }
 
-int tomoReconPoll(int *pReconComplete, int *pSlicesRemaining)
+static void workerTask(workerCreateStruct *pWCS)
 {
-  *pReconComplete = reconComplete;
-  *pSlicesRemaining = slicesRemaining;
-  return 0;
+  pWCS->pTomoRecon->workerTask(pWCS->doneEventId);
+  free(pWCS);
 }
+} // extern "C"
 
-int tomoReconAbort()
-{
-  if (!pTomoSupervisor) {
-    fprintf(debugFile, "tomoReconAbort: tomoSupervisor object does not exist\r\n"); 
-    fflush(debugFile);
-    return -1;
-  }
-  pTomoSupervisor->abort();
-  return 0;
-}
 
-static void supervisorTask(void *drvPvt)
-{
-  tomoSupervisor *pTomoSupervisor = (tomoSupervisor *)drvPvt;
-  pTomoSupervisor->supervisorTask();
-}
-
-static void workerTask(void *drvPvt)
-{
-  tomoWorker *pTomoWorker = (tomoWorker *)drvPvt;
-  pTomoWorker->workerTask();
-}
-
-}  // extern "C"
-
-/** Constructor for the tomoSupervisor class.
+/** Constructor for the tomoRecon class.
 * \param[in] pTomoParams A structure containing the tomography reconstruction parameters
 * \param[in] pTomoParams Pointer to angles
 * \param[in] pInput Pointer to input data
 * \param[out] pOutput Pointer to output data */
-tomoSupervisor::tomoSupervisor(tomoParams_t *pTomoParams, float *pAngles, float *pInput, float *pOutput)
-  : 
+tomoRecon::tomoRecon(tomoParams_t *pTomoParams, float *pAngles, float *pInput, float *pOutput)
+  : reconComplete_(0),
+    slicesRemaining_(pTomoParams->numSlices),
     pTomoParams_(pTomoParams),
     numPixels_(pTomoParams_->numPixels),
     numSlices_(pTomoParams_->numSlices),
@@ -84,7 +51,10 @@ tomoSupervisor::tomoSupervisor(tomoParams_t *pTomoParams, float *pAngles, float 
     numThreads_(pTomoParams_->numThreads),
     pAngles_(pAngles),
     pInput_(pInput),
-    pOutput_(pOutput)
+    pOutput_(pOutput),
+    queueElements_((pTomoParams_->numSlices+1)/2),
+    debug_(pTomoParams_->debug),
+    shutDown_(0)
 
 {
   epicsThreadId supervisorTaskId;
@@ -94,22 +64,15 @@ tomoSupervisor::tomoSupervisor(tomoParams_t *pTomoParams, float *pAngles, float 
   int nextSlice=0;
   int i;
   int status;
-  
-  // Set static variables that need to outlive the object
-  tomoAbort = 0;
-  tomoDebug = pTomoParams_->debug;
-  pTomoSupervisor = this;
-  reconComplete = 0;
-  slicesRemaining = pTomoParams_->numSlices;
-  
-  debugFile = stdout;
+  static const char *functionName="tomoRecon::tomoRecon";
+
+  debugFile_ = stdout;
   #ifdef _WIN32
-  if (tomoDebug) {
-    debugFile = fopen("tomoReconDebug.out", "w");
+  if (debug_) {
+    debugFile_ = fopen("tomoReconDebug.out", "w");
   }
   #endif
-  
-  queueElements_ = (pTomoParams_->numSlices+1)/2;
+ 
   toDoQueue_ = epicsMessageQueueCreate(queueElements_, sizeof(toDoMessage_t));
   doneQueue_ = epicsMessageQueueCreate(queueElements_, sizeof(doneMessage_t));
   workerDoneEventIds_ = (epicsEventId *) malloc(numThreads_ * sizeof(epicsEventId));
@@ -136,8 +99,8 @@ tomoSupervisor::tomoSupervisor(tomoParams_t *pTomoParams, float *pAngles, float 
     }
     status = epicsMessageQueueTrySend(toDoQueue_, &toDoMessage, sizeof(toDoMessage));
     if (status) {
-      fprintf(debugFile, "tomoRecon::supervisorTask, error calling epicsMessageQueueTrySend, status=%d\r\n", status);
-      fflush(debugFile);
+      logMsg("%s:, error calling epicsMessageQueueTrySend, status=%d", 
+          functionName, status);
     }
   }
 
@@ -148,116 +111,100 @@ tomoSupervisor::tomoSupervisor(tomoParams_t *pTomoParams, float *pAngles, float 
                                 (EPICSTHREADFUNC) ::supervisorTask,
                                 this);
   if (supervisorTaskId == 0) {
-    fprintf(debugFile, "tomoRecon::tomoRecon epicsThreadCreate failure for supervisorTask\r\n"); 
-    fflush(debugFile);
-    return;
+    logMsg("%s: epicsThreadCreate failure for supervisorTask", functionName); 
   }
 }
 
-/** Destructor for the tomoSupervisor class. */
-tomoSupervisor::~tomoSupervisor() 
+/** Destructor for the tomoRecon class. */
+tomoRecon::~tomoRecon() 
 {
   int i;
   
-  pTomoSupervisor = NULL;  
   epicsMessageQueueDestroy(toDoQueue_);
   epicsMessageQueueDestroy(doneQueue_);
   for (i=0; i<numThreads_; i++) epicsEventDestroy(workerDoneEventIds_[i]);
   epicsMutexDestroy(fftwMutexId_);
   free(workerDoneEventIds_);
-  if (debugFile != stdout) fclose(debugFile);
+  if (debugFile_ != stdout) fclose(debugFile_);
 }
 
-void tomoSupervisor::abort()
+void tomoRecon::poll(int *pReconComplete_, int *pSlicesRemaining_)
 {
-  // Set the abort flag, which will stop the supervisorTask and workerTask threads
-  tomoAbort = 1;
+  *pReconComplete_ = reconComplete_;
+  *pSlicesRemaining_ = slicesRemaining_;
+}
+
+void tomoRecon::abort()
+{
+  // Set the shutDown flag, which will stop the supervisorTask and workerTask threads
+  shutDown_ = 1;
 }
 
 /** Supervisor control task that runs as a separate thread. */
-void tomoSupervisor::supervisorTask(void)
+void tomoRecon::supervisorTask(void)
 {
   int i;
   int status;
   doneMessage_t doneMessage;
+  char workerTaskName[20];
+  epicsThreadId workerTaskId;
+  workerCreateStruct *pWCS;
+  static const char *functionName="tomoRecon::supervisorTask";
 
-  /* Create the worker objects */
+  /* Create the worker tasks */
   for (i=0; i<numThreads_; i++) {
     workerDoneEventIds_[i] = epicsEventCreate(epicsEventEmpty);
-    new tomoWorker(this, i, workerDoneEventIds_[i]);
+    sprintf(workerTaskName, "workerTask%d", i);
+    pWCS = (workerCreateStruct *)malloc(sizeof(workerCreateStruct));
+    pWCS->pTomoRecon = this;
+    pWCS->doneEventId = workerDoneEventIds_[i];
+    workerTaskId = epicsThreadCreate(workerTaskName,
+                       epicsThreadPriorityMedium,
+                       epicsThreadGetStackSize(epicsThreadStackMedium),
+                       (EPICSTHREADFUNC) ::workerTask,
+                       pWCS);
+    if (workerTaskId == 0) {
+      logMsg("%s epicsThreadCreate failure for workerTask %d", functionName, i);
+      return;
+    }
   }
   
   // Wait for all the messages to come back, indicating that reconstruction is complete
-  while (slicesRemaining > 0) {
-    if (tomoAbort) break;
-    status = epicsMessageQueueReceiveWithTimeout(doneQueue_, &doneMessage, sizeof(doneMessage), 1.0);
+  while (slicesRemaining_ > 0) {
+    if (shutDown_) break;
+    status = epicsMessageQueueReceive(doneQueue_, &doneMessage, sizeof(doneMessage));
     if (status != sizeof(doneMessage)) {
-      fprintf(debugFile, "tomoRecon::supervisorTask, timeout waiting for worker thread message\r\n");
-      fflush(debugFile);
+      logMsg("%s, error reading worker thread message", functionName);
       continue;
     }
-    slicesRemaining -= doneMessage.numSlices;
+    slicesRemaining_ -= doneMessage.numSlices;
   }
-  reconComplete = 1;
-  // Wait for the worker threads to exit before deleting the message queues, etc.
+  if (debug_) logMsg("%s: All slices complete!", functionName);
+  // Wait for the worker threads to exit before setting the reconstruction complete flag.
   // They will exit because the toDoQueue is now empty
   for (i=0; i<numThreads_; i++) {
+    if (debug_) logMsg("%s: Beginning wait for worker task %d to complete, eventId=%p", 
+          functionName, i, workerDoneEventIds_[i]);
     status = epicsEventWaitWithTimeout(workerDoneEventIds_[i], 1.0);
+    if (debug_) logMsg("%s: Done wait for worker task %d to complete, eventId=%p, status=%d", 
+          functionName, i, workerDoneEventIds_[i], status);
     if (status != epicsEventWaitOK) {
-      fprintf(debugFile, "Error waiting for worker task %d to complete ...\r\n", i);
-      fflush(debugFile);
+      logMsg("%s: Error waiting for worker task %d to complete, eventId=%p, status=%d", 
+          functionName, i, workerDoneEventIds_[i], status);
     }
   }
-      
-  delete this;
+  reconComplete_ = 1;
+  if (debug_) logMsg("%s: Reconstruction complete! Exiting supervisor task.", functionName);
 }
-
-
-
-/** Constructor for the tomoWorker class.
-* \param[in] pTomoSupervisor - pointer to tomoSupervisor object
-* \param[in] taskNum - index number of this worker task
-*/
-  tomoWorker::tomoWorker(tomoSupervisor *pTomoSupervisor, int taskNum, epicsEventId doneEventId)
-  : 
-    pTS_(pTomoSupervisor),
-    taskNum_(taskNum),
-    doneEventId_(doneEventId)
-{
-  epicsThreadId workerTaskId;
-  char workerTaskName[20];
-  static const char *functionName = "tomoWorker::tomoWorker";
-  
-  sprintf(workerTaskName, "workerTask%d", taskNum_+1);
-  workerTaskId = epicsThreadCreate(workerTaskName,
-                     epicsThreadPriorityMedium,
-                     epicsThreadGetStackSize(epicsThreadStackMedium),
-                     (EPICSTHREADFUNC) ::workerTask,
-                     this);
-  if (workerTaskId == 0) {
-    fprintf(debugFile, "%s epicsThreadCreate failure for workerTask %d\r\n", functionName, taskNum_+1);
-    fflush(debugFile);
-    return;
-  }
-}
-
-/** Destructor for the tomoWorker class. */
-tomoWorker::~tomoWorker() 
-{
-}
-
 
 /** Worker task that runs as a separate thread. Multiple worker tasks can be running simultaneously.
  * Each worker task reconstructs slices that it get from the toDoQueue. */
-void tomoWorker::workerTask(void)
+void tomoRecon::workerTask(epicsEventId doneEventId)
 {
   toDoMessage_t toDoMessage;
   doneMessage_t doneMessage;
   epicsTimeStamp tStart, tStop;
   long imageSize;
-  int numProjections = pTS_->numProjections_;
-  int paddedWidth = pTS_->paddedWidth_;
-  int numPixels = pTS_->numPixels_;
   int status;
   float *pOut;
   int i;
@@ -267,37 +214,38 @@ void tomoWorker::workerTask(void)
   grid_struct gridStruct;
   float **S1, **S2, **R1, **R2;
   grid *pGrid;
+  static const char *functionName="tomoRecon::workerTask";
   
-  sgStruct.n_ang    = numProjections;
-  sgStruct.n_det    = paddedWidth;
-  sgStruct.geom     = pTS_->pTomoParams_->geom;
-  sgStruct.angles   = pTS_->pAngles_;
-  sgStruct.center   = pTS_->pTomoParams_->centerOffset;
-  get_pswf(pTS_->pTomoParams_->pswfParam, &gridStruct.pswf);
-  gridStruct.sampl     = pTS_->pTomoParams_->sampl;
-  gridStruct.R         = pTS_->pTomoParams_->R;
-  gridStruct.MaxPixSiz = pTS_->pTomoParams_->MaxPixSiz;
-  gridStruct.X0        = pTS_->pTomoParams_->X0;
-  gridStruct.Y0        = pTS_->pTomoParams_->Y0;
-  gridStruct.ltbl      = pTS_->pTomoParams_->ltbl;
-  gridStruct.filter    = get_filter(pTS_->pTomoParams_->fname);
-  gridStruct.verbose   = tomoDebug;
+  sgStruct.n_ang    = numProjections_;
+  sgStruct.n_det    = paddedWidth_;
+  sgStruct.geom     = pTomoParams_->geom;
+  sgStruct.angles   = pAngles_;
+  sgStruct.center   = pTomoParams_->centerOffset + (paddedWidth_ - numPixels_)/2;
+  get_pswf(pTomoParams_->pswfParam, &gridStruct.pswf);
+  gridStruct.sampl     = pTomoParams_->sampl;
+  gridStruct.R         = pTomoParams_->R;
+  gridStruct.MaxPixSiz = pTomoParams_->MaxPixSiz;
+  gridStruct.X0        = pTomoParams_->X0;
+  gridStruct.Y0        = pTomoParams_->Y0;
+  gridStruct.ltbl      = pTomoParams_->ltbl;
+  gridStruct.filter    = get_filter(pTomoParams_->fname);
+  gridStruct.verbose   = debug_;
 
   // Must take a mutex when creating grid object, because it creates fftw plans, which is not thread safe
-  epicsMutexLock(pTS_->fftwMutexId_);
+  epicsMutexLock(fftwMutexId_);
   pGrid = new grid(&gridStruct, &sgStruct, &imageSize);
-  epicsMutexUnlock(pTS_->fftwMutexId_);
+  epicsMutexUnlock(fftwMutexId_);
 
-  sinOffset = (imageSize - numPixels)/2;
+  sinOffset = (imageSize - numPixels_)/2;
   if (sinOffset < 0) sinOffset = 0;
 
-  sin1   = (float *) calloc(paddedWidth * numProjections, sizeof(float));
-  sin2   = (float *) calloc(paddedWidth * numProjections, sizeof(float));
-  air    = (float *) malloc(paddedWidth*sizeof(float));
+  sin1   = (float *) calloc(paddedWidth_ * numProjections_, sizeof(float));
+  sin2   = (float *) calloc(paddedWidth_ * numProjections_, sizeof(float));
+  air    = (float *) malloc(paddedWidth_*sizeof(float));
   recon1 = (float *) calloc(imageSize * imageSize, sizeof(float));
   recon2 = (float *) calloc(imageSize * imageSize, sizeof(float));  
-  S1     = (float **) malloc(numProjections * sizeof(float *));
-  S2     = (float **) malloc(numProjections * sizeof(float *));
+  S1     = (float **) malloc(numProjections_ * sizeof(float *));
+  S2     = (float **) malloc(numProjections_ * sizeof(float *));
   R1     = (float **) malloc(imageSize * sizeof(float *));
   R2     = (float **) malloc(imageSize * sizeof(float *));
 
@@ -306,9 +254,9 @@ void tomoWorker::workerTask(void)
      Need to build those tables */
   S1[0] = sin1;
   S2[0] = sin2;
-  for (i=1; i<numProjections; i++) {
-    S1[i] = S1[i-1] + paddedWidth;
-    S2[i] = S2[i-1] + paddedWidth;
+  for (i=1; i<numProjections_; i++) {
+    S1[i] = S1[i-1] + paddedWidth_;
+    S2[i] = S2[i-1] + paddedWidth_;
   }
   R1[0] = recon1;
   R2[0] = recon2;
@@ -318,11 +266,10 @@ void tomoWorker::workerTask(void)
   }
 
   while(1) {
-    status = epicsMessageQueueTryReceive(pTS_->toDoQueue_, &toDoMessage, sizeof(toDoMessage));
+    status = epicsMessageQueueTryReceive(toDoQueue_, &toDoMessage, sizeof(toDoMessage));
     if (status == -1) break;
     if (status != sizeof(toDoMessage)) {
-      fprintf(debugFile, "tomoRecon::workerTask, error calling epicsMessageQueueReceive, status=%d\r\n", status);
-      fflush(debugFile);
+      logMsg("%s:, error calling epicsMessageQueueReceive, status=%d", functionName, status);
       break;
     }
     epicsTimeGetCurrent(&tStart);
@@ -338,32 +285,35 @@ void tomoWorker::workerTask(void)
     epicsTimeGetCurrent(&tStart);
     pGrid->recon(S1, S2, &R1, &R2);
     // Copy to output array, discard padding
+if (debug_) logMsg("%s: %s doing memcpy slice %d, pOut=%p, sinOffset=%d, imageSize=%d",
+functionName, epicsThreadGetNameSelf(), toDoMessage.sliceNumber, toDoMessage.pOut1, sinOffset, imageSize);
     for (i=0, pOut=toDoMessage.pOut1, pRecon=recon1+sinOffset*imageSize; 
-         i<imageSize;
-         i++, pOut+=numPixels, pRecon+=imageSize) {
-      memcpy(pOut, pRecon+sinOffset, imageSize*sizeof(float));
+         i<numPixels_;
+         i++, pOut+=numPixels_, pRecon+=imageSize) {
+      memcpy(pOut, pRecon+sinOffset, numPixels_*sizeof(float));
     }
+if (debug_) logMsg("%s: %s doing memcpy slice %d, pOut=%p, sinOffset=%d, imageSize=%d",
+functionName, epicsThreadGetNameSelf(), toDoMessage.sliceNumber+1, toDoMessage.pOut2, sinOffset, imageSize);
     if (doneMessage.numSlices == 2) {
       for (i=0, pOut=toDoMessage.pOut2, pRecon=recon2+sinOffset*imageSize; 
-           i<imageSize;
-           i++, pOut+=numPixels, pRecon+=imageSize) {
-      memcpy(pOut, pRecon+sinOffset, imageSize*sizeof(float));
+           i<numPixels_;
+           i++, pOut+=numPixels_, pRecon+=imageSize) {
+        memcpy(pOut, pRecon+sinOffset, numPixels_*sizeof(float));
       }
     }
     epicsTimeGetCurrent(&tStop);
     doneMessage.reconTime = epicsTimeDiffInSeconds(&tStop, &tStart);
     doneMessage.sliceNumber = toDoMessage.sliceNumber;
-    status = epicsMessageQueueTrySend(pTS_->doneQueue_, &doneMessage, sizeof(doneMessage));
+    status = epicsMessageQueueTrySend(doneQueue_, &doneMessage, sizeof(doneMessage));
     if (status) {
-      printf("tomoRecon::workerTask, error calling epicsMessageQueueTrySend, status=%d\r\n", status);
+      printf("%s, error calling epicsMessageQueueTrySend, status=%d", functionName, status);
     }
-    if (tomoDebug > 0) { 
-      fprintf(debugFile, "tomoRecon::workerTask, thread=%s, slice=%d, sinogram time=%f, recon time=%f\r\n", 
-          epicsThreadGetNameSelf(), doneMessage.sliceNumber, 
+    if (debug_ > 0) { 
+      logMsg("%s:, thread=%s, slice=%d, sinogram time=%f, recon time=%f", 
+          functionName, epicsThreadGetNameSelf(), doneMessage.sliceNumber, 
           doneMessage.sinogramTime, doneMessage.reconTime);
-      fflush(debugFile);
     }
-    if (tomoAbort) break;
+    if (shutDown_) break;
   }
   free(sin1);
   free(sin2);
@@ -374,57 +324,53 @@ void tomoWorker::workerTask(void)
   free(S2);
   free(R1);
   free(R2);
-  if (tomoDebug > 0) {
-    fprintf(debugFile, "tomoRecon::workerTask %s exiting\r\n", epicsThreadGetNameSelf());
-    fflush(debugFile);
+  if (debug_ > 0) {
+    logMsg("tomoRecon::workerTask %s exiting, eventId=%p", 
+        epicsThreadGetNameSelf(), doneEventId);
   }
+  delete pGrid;
   // Send an event so the supervisor knows this thread is done
-  epicsEventSignal(doneEventId_);
-  delete this;
+  epicsEventSignal(doneEventId);
 }
 
-void tomoWorker::sinogram(float *pIn, float *pOut, float *air)
+void tomoRecon::sinogram(float *pIn, float *pOut, float *air)
 {
   int i, j;
-  int numAir = pTS_->pTomoParams_->airPixels;
-  int numProjections = pTS_->numProjections_;
-  int paddedWidth = pTS_->paddedWidth_;
-  int numPixels = pTS_->numPixels_;
-  int numSlices = pTS_->numSlices_;
-  int sinOffset = (paddedWidth - numPixels)/2;
+  int numAir = pTomoParams_->airPixels;
+  int sinOffset = (paddedWidth_ - numPixels_)/2;
   float airLeft, airRight, airSlope, ratio;
   float *pInData;
   float *pOutData;
   
   for (i=0, pInData=pIn, pOutData=pOut; 
-       i<numProjections;
-       i++, pInData+=numPixels*numSlices, pOutData+=paddedWidth) {
+       i<numProjections_;
+       i++, pInData+=numPixels_*numSlices_, pOutData+=paddedWidth_) {
     if (numAir > 0) {
       for (j=0, airLeft=0, airRight=0; j<numAir; j++) {
         airLeft += pInData[j];
-        airRight += pInData[numPixels - 1 - j];
+        airRight += pInData[numPixels_ - 1 - j];
       }
       airLeft /= numAir;
       airRight /= numAir;
       if (airLeft <= 0.) airLeft = 1.;
       if (airRight <= 0.) airRight = 1.;
-      airSlope = (airRight - airLeft)/(numPixels - 1);
+      airSlope = (airRight - airLeft)/(numPixels_ - 1);
     }
     else {
       // Assume data has been normalized to air=10000
       airLeft = 1e4;
       airSlope = 0;
     }
-    for (j=0; j<numPixels; j++) {
+    for (j=0; j<numPixels_; j++) {
        air[j] = airLeft + airSlope*i;
     }
-    if (pTS_->pTomoParams_->fluorescence) {
-      for (j=0; j<numPixels; j++) {
+    if (pTomoParams_->fluorescence) {
+      for (j=0; j<numPixels_; j++) {
         pOutData[sinOffset + j] = pInData[j];
       }
     }
     else {
-      for (j=0; j<numPixels; j++) {
+      for (j=0; j<numPixels_; j++) {
         ratio = pInData[j]/air[j];
         if (ratio <= 0.) ratio = 1.;
         pOutData[sinOffset + j] = -log(ratio);
@@ -433,4 +379,21 @@ void tomoWorker::sinogram(float *pIn, float *pOut, float *air)
   }
 }
 
+void tomoRecon::logMsg(const char *pFormat, ...)
+{
+    va_list     pvar;
+    epicsTimeStamp now;
+    char nowText[40];
+
+    epicsTimeGetCurrent(&now);
+    nowText[0] = 0;
+    epicsTimeToStrftime(nowText,sizeof(nowText),
+         "%Y/%m/%d %H:%M:%S.%03f",&now);
+    fprintf(debugFile_,"%s ",nowText);
+    va_start(pvar,pFormat);
+    vfprintf(debugFile_, pFormat, pvar);
+    va_end(pvar);
+    fprintf(debugFile_, "\r\n");
+    fflush(debugFile_);
+}
 
